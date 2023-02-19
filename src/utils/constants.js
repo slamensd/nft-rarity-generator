7c8a54cb9969ce30bd9e6881042e7e3ccc3d1cc7
slamensd <58567346+slamensd@users.noreply.github.com>
2023-02-19 02:25:10 +0000

    tak

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -4,19 +4,19 @@ dotenv.config();
 import { createErrorContainer } from './errorContainer.js';
 import { createSpinner } from './spinner.js';
 
-export const ALCHEMY_API_KEY = process.env.ALCHEMY_API_KEY;
+export const ALCHEMY_API_KEY = UVP4Hy78DWaOdNYtzBbnBpnN3Sc_L9XU;
 
 if (!ALCHEMY_API_KEY) {
   throw new Error('ALCHEMY_API_KEY is required!');
 }
 
-export const CONTRACT_ADDRESS = process.env['CONTRACT_ADDRESS'];
+export const CONTRACT_ADDRESS = 0x68Bd8b7C45633de6d7AFD0B1F7B86b37B8a3C02A;
 
 if (!CONTRACT_ADDRESS) {
   throw new Error(`${CONTRACT_ADDRESS} must be defined!`);
 }
 
-export const MONGO_DB_URL = process.env.MONGO_DB_URL;
+export const MONGO_DB_URL = "mongodb+srv://hello:Supson08@bbfc.7d41q.mongodb.net/?retryWrites=true&w=majority";
 
 if (!MONGO_DB_URL) {
   throw new Error(`${MONGO_DB_URL} must be defined!`);
